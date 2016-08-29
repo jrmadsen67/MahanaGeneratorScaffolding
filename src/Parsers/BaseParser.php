b63741063c4b390078a97d1c2d2fc25f11d243da
Jeff Madsen <jrmadsen67@gmail.com>
2016-08-29 01:41:56 +0000

    add stub to config items

--- src/Parsers/BaseParser.php
+++ src/Parsers/BaseParser.php
@@ -10,11 +10,15 @@ class BaseParser{
 
     public $name;
 
+    public $stub;
+
     function __construct($itemArray)
     {
         $this->itemArray = new Collection($itemArray);
 
         $this->setName();
+
+        $this->setStub();
     }
 
     function getName(){
@@ -23,5 +27,13 @@ class BaseParser{
 
     function setName(){
         $this->name = $this->itemArray->get('name', '');
+    }
+
+    function getStub(){
+        return $this->stub;
+    }
+
+    function setStub(){
+        $this->stub = $this->itemArray->get('stub', '');
     }
 }
